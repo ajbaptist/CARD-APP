3c1fa0bcd51ffdf1cd501b7070378228c085e97a
john baptist <ajbaptist18@gmail.com>
2021-07-20 20:57:57 +0100

    commit

--- card_app/lib/main.dart
+++ card_app/lib/main.dart
@@ -92,6 +92,36 @@ class _HomeState extends State<Home> {
                   ),
                 ),
                 TransectionContainer(),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(20),
+                    child: Container(
+                        height: 200,
+                        color: Colors.white,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                          children: [
+                            Image.asset(
+                              'assets/pic.jpg',
+                              height: 150,
+                              width: double.infinity,
+                              fit: BoxFit.cover,
+                            ),
+                            Text(
+                              'Appolo pharmacy',
+                              style: blackClr,
+                              textAlign: TextAlign.center,
+                            ),
+                            Text(
+                              'upto 15% off at appolo pharmacy',
+                              style: greyClr,
+                              textAlign: TextAlign.center,
+                            )
+                          ],
+                        )),
+                  ),
+                )
               ],
             ),
           ))
